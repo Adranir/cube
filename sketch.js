31587a34f822e0cc85f4f5eaeb57a9c0e878f224
Adranir <nath.hercher@gmail.com>
2018-05-01 17:31:35 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+function setup() {
+  print("Sur cette photo, j'ai voulu montrer ma vision du monde, et comment je me sens au quotidien, c'est pour cela que j'ai mis un effet de flou sur la photo, et qu'elle est légèrement de travers.");
+  print("J'ai aussi voulu montrer ma passion, on peut donc voir un ordinateur, et des puzzles type rubik's cube.");
+  print("J'ai assombri la photo, encore une fois pour montrer ma vision du monde, j'essayais de faire ressortir une emotion de nostalgie, de solitude, et donner l'impression d'être perdu dans le pièce, qui elle représente mon monde.");
+  print("La photo est aussi prise dans l'intimité (chambre et pyjama), pour que le tout soit le plus personnel possible");
+}
