21b2b4987f90472e98478885f17fe0c60e0b9a52
Adranir <nath.hercher@gmail.com>
2018-05-01 17:34:50 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,4 +3,6 @@ function setup() {
   print("J'ai aussi voulu montrer ma passion, on peut donc voir un ordinateur, et des puzzles type rubik's cube.");
   print("J'ai assombri la photo, encore une fois pour montrer ma vision du monde, j'essayais de faire ressortir une emotion de nostalgie, de solitude, et donner l'impression d'être perdu dans le pièce, qui elle représente mon monde.");
   print("La photo est aussi prise dans l'intimité (chambre et pyjama), pour que le tout soit le plus personnel possible");
+  print("Enfin, pour le mode d'exposition, j'ai choisi tout d'abord de créer un site, encore une fois, ma passion pour l'informatique, puis j'ai choisi d'en faire une sorte de jeu de piste, car je suis friant d'enigme et de recherche");
+  print("J'ai aussi choisi de garder les pages très simplistes, car je ne voulais rien ajouter de superficiel et je voulais garder mon travail aussi personnel que possible");
 }
